2040b2e5df0c647c4868cd3c9c5bd5929040651f
MarcosNSouza87 <m_souza@outlook.com.br>
2024-11-30 15:05:44 -0300

    finish_screen_home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { ExerciseCard } from '@components/ExerciseCard';
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
 import * as GS from '@gluestack-ui/themed';
@@ -5,17 +6,32 @@ import { useState } from 'react';
 import { FlatList } from 'react-native';
 
 export function Home() {
+	const [exercises, setExercises] = useState([
+		'Puxada Frontal',
+		'Remada Curvada',
+		'Remada unilateral',
+		'Flexao de braco',
+		'Levantamento Frontal',
+		'1Puxada Frontal',
+		'2Remada Curvada',
+		'3Remada unilateral',
+		'4Flexao de braco',
+		'5Levantamento Frontal',
+	]);
+
 	const [groups, setGroups] = useState(['Costas', 'Biceps', 'Tríceps', 'Ombro']);
+
 	const [groupSelected, setGroupSelected] = useState('Costas');
+
 	return (
 		<GS.VStack flex={1}>
 			<HomeHeader />
 			<FlatList
 				data={groups}
 				keyExtractor={(item) => item}
-        horizontal
-        contentContainerStyle={{paddingHorizontal: 32}}
-        style={{marginVertical: 40, maxHeight: 44, minHeight: 44}}
+				horizontal
+				contentContainerStyle={{ paddingHorizontal: 32 }}
+				style={{ marginVertical: 40, maxHeight: 44, minHeight: 44 }}
 				renderItem={({ item }) => (
 					<Group
 						name={item}
@@ -24,6 +40,23 @@ export function Home() {
 					/>
 				)}
 			/>
+			<GS.VStack px="$8">
+				<GS.HStack justifyContent="space-between" mb="$5" alignItems="center">
+					<GS.Heading color="$gray200" fontSize="$md" fontFamily="$heading">
+						Exercícios
+					</GS.Heading>
+					<GS.Text color="$gray200" fontSize="$sm" fontFamily="$body">
+						{exercises.length}
+					</GS.Text>
+				</GS.HStack>
+				<FlatList
+					data={exercises}
+					keyExtractor={(item) => item}
+					renderItem={() => <ExerciseCard />}
+					showsVerticalScrollIndicator={false}
+					contentContainerStyle={{ paddingBottom: 20 }}
+				/>
+			</GS.VStack>
 		</GS.VStack>
 	);
 }
